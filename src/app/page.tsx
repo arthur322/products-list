245fbe03b00256acf4d97523d5903c191b31b4df
Arthur Lima <arthur-esc@hotmail.com>
2024-03-04 20:09:02 -0300

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 import { BackButton } from '@/components/back-button'
 import { PageTitle } from '@/components/page-title'
 import { Search } from '@/components/search'
+import { Footer } from '@/components/footer'
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      <header className="flex gap-4 mb-10">
-        <BackButton />
-        <PageTitle>Institucional</PageTitle>
-      </header>
-      <main className="">
-        <div className="w-[328px]">
-          <Search />
-        </div>
-      </main>
-    </div>
+    <>
+      <div className="container mx-auto p-10 min-h-[calc(100vh-56px)]">
+        <header className="flex gap-4 mb-10">
+          <BackButton />
+          <PageTitle>Institucional</PageTitle>
+        </header>
+        <main className="">
+          <div className="w-[328px]">
+            <Search />
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,5 @@
+export function Footer() {
+  return (
+    <footer className="center h-14 bg-gray-200 mt-auto">Squadfy © 2024</footer>
+  )
+}
